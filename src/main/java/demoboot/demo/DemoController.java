e7ab66bf177a3cfe0152604c25e31967bacaf9ce
Bin Li <binblee@gmail.com>
2021-01-05 20:32:07 +0800

    init commit

---
+++ src/main/java/demoboot/demo/DemoController.java
@@ -0,0 +1,13 @@
+package demoboot.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    String greetings(){
+        return "Hello World!";
+    }
+}
